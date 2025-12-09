fca5a166c89e5b3661c3102637b8511a3b38b9c2
Mattia Mion <mattia.mion@gmail.com>
2025-12-09 15:51:19 +0100

    feat: 01 done

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -2,12 +2,20 @@
 
 const userName = 'Mario';
 
+const inputName = prompt('inserisci nome')
 
 // Dichiara la funzione qui.
 
+/*function sayHello(mmName) {
+return "Ciao " + mmName
+}*/
+
+//funzione arrow
+
+const sayHello = (mmName) => 'Ciao ' + mmName
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(sayHello(inputName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario

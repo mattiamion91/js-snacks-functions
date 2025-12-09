d1f4fd942cc61c7b5a838c81171d4b3915cfb7bf
Mattia Mion <mattia.mion@gmail.com>
2025-12-09 17:17:07 +0100

    feat 02 done

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,31 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function extractInitial(myNames) {
+    //array che conterrá tutte le iniziali
+    const initial = [];
 
+    //ciclo che mi permette di scorrere tutti gli elementi dell'array
+    for (let i = 0; i < myNames.length; i++) {
+        //considero il nome corrente
+        const currentName = myNames[i];
+
+        //normalizzo input, lo trasformo in una stringa
+        const cleanName = String(currentName);
+
+        //estraggo la prima lettera
+        const firstLetter = cleanName[0];
+
+        //aggiungo la prima lettera all' array vuoto che ho creato
+        initial.push(firstLetter);
+    }
+//restituisco l'array con tutte le iniziali
+    return initial;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(extractInitial(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
